3983311cad653dbbf16c7395ec2ad5f878dfc682
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-01-03 16:32:06 +0100

    adjusts margin up of title and text to improve mobile

--- src/app/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import React from "react";
-
-export const Logo = () => (
-    <Image src="/d&d.png" alt="dungeouns and dragons" width={240} height={240} className="cursor-pointer" />
-);
-
