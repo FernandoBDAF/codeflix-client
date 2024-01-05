afb28c9a32e93bd614d97a15a88e182b840c5772
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-01-05 16:38:23 +0100

    files route and LoginValidation working

--- src/app/auth/login/api/route.ts
+++ src/app/auth/login/api/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import { loginFormSchema } from "@/app/lib/validations/LoginValidation";
+import { LoginFormSchema } from "@/app/lib/validations/LoginValidation";
 
 export async function POST(request: NextRequest) {
     try {
-        const { email, password } = loginFormSchema.parse(await request.json());
+        const { email, password } = LoginFormSchema.parse(await request.json());
         console.log(email, password);
-        return new NextResponse("It workds! 🎉")
-    } catch (err: any) {
-        console.log(err.message);
-        return new NextResponse(err.message, { status: 400 });
+        return new NextResponse("It workds!")
+    } catch (error: any) {
+        console.log(error.message);
+        return new NextResponse(error.message, { status: 400 });
     }
 }
