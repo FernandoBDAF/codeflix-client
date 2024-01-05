8e3f36081aa5c56d799ed5d2d89e641da5f140fe
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-01-05 17:14:34 +0100

    Login form parcialmente completo e buildando

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -4,15 +4,17 @@ import React, { FormEvent, useState } from "react";
 import { AuthForm } from "@/app/components/AuthForm";
 import { useRouter } from "next/navigation";
 
-// type ServerError = {
-//     message: string;
-// }
+type ServerError = {
+    message: string;
+}
 
 export default function LoginForm() {
+    const [errors, setErrors] = useState<string[]>([]);
     // const router = useRouter();
-    // const { errors, setErrors } = useState<string[]>([]);
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        
+
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         const email = formData.get("email");
@@ -24,15 +26,19 @@ export default function LoginForm() {
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({ email, password }),
             });
+
+            if (response.ok) {
+                // router.push("/");
+                // return;
+            }
+
+            const payload: ServerError[] = await response.json();
+            setErrors(payload.map((error) => error.message));
         } catch (error) {
             console.error(error);
+            setErrors(["An unknown error occurred."]);
         }
     }
-
-    //         if (response.ok) {
-    //             router.push("/");
-    //             return;
-    //         }
 
     //         const payload: ServerError[] = await response.json();
     //         setErrors(payload.map((error) => error.message));
